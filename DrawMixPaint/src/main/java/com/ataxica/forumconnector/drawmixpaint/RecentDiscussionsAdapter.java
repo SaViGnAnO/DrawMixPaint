d17ff2bbdc34a4f0294b797b56ed369cdcb36ba7
Brian Savignano <brian@ataxica.com>
2014-06-03 14:05:56 -0500

    beginning programming of custom arrayadapter for DiscussionList

---
+++ DrawMixPaint/src/main/java/com/ataxica/forumconnector/drawmixpaint/RecentDiscussionsAdapter.java
@@ -0,0 +1,34 @@
+package com.ataxica.forumconnector.drawmixpaint;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Brian Savignano on 6/2/2014.
+ */
+public class RecentDiscussionsAdapter extends BaseAdapter {
+
+    private ArrayList<MessageDetails>
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
