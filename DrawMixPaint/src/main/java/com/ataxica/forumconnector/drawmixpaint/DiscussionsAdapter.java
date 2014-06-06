c61ea81af2aea1a6bf3e3a0bafb4a1805ed97e7e
Brian Savignano <ataxica@gmail.com>
2014-06-06 12:03:26 -0500

    working on comments view/adapter and OnClickListener for lists

--- DrawMixPaint/src/main/java/com/ataxica/forumconnector/drawmixpaint/DiscussionsAdapter.java
+++ DrawMixPaint/src/main/java/com/ataxica/forumconnector/drawmixpaint/DiscussionsAdapter.java
@@ -6,6 +6,7 @@ import android.opengl.Visibility;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -76,5 +77,9 @@ public class DiscussionsAdapter extends ArrayAdapter<DiscussionDetails> {
         }
 
         return v;
+    }
+
+    public static abstract class OnClickListener implements View.OnClickListener {
+        
     }
 }
